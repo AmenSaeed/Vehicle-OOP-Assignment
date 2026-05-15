8338359ddae477008a37825a3ed48352f72e60f7
Amen <amnsaidd@gmail.com>
2026-05-15 12:02:44 +0300

    New Batman Version

---
+++ BatmanProject.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Amen Saeed on 15/05/2026.
+//
+
+#include "BatmanProject.h"
---
+++ BatmanProject.h
@@ -0,0 +1,16 @@
+//
+// Created by Amen Saeed on 15/05/2026.
+//
+
+#ifndef INC_01_ARRAY_BATMANPROJECT_H
+#define INC_01_ARRAY_BATMANPROJECT_H
+
+
+
+class BatmanProject {
+
+};
+
+
+
+#endif //INC_01_ARRAY_BATMANPROJECT_H
